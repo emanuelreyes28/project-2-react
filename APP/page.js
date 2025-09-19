9c41135634c6506d7735d8d37e2000d9ba3fd169
Emanuel Reyes <grimmy@Emanuels-MacBook-Air.local>
2025-09-18 23:30:21 -0400

    React App

---
+++ APP/page.js
@@ -0,0 +1,27 @@
+
+import LikeButton from './like-button';
+ 
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+ 
+function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+ 
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+       
+        export default function HomePage() {
+        }
+
+      </ul>
+ 
+      <LikeButton />
+    </div>
+  );
+}
